ab3cc4ac9a446e91c9a5f3b0e6978d540d5cb425
adam salem <adam.salem@adp.com>
2014-01-13 18:07:39 -0500

    test

--- rakefile.rb
+++ rakefile.rb
@@ -3,4 +3,22 @@ require 'ci/reporter/rake/rspec'
 
 RSpec::Core::RakeTask.new(:all => ["ci:setup:rspec"]) do |t|
   t.pattern = '**/*_spec.rb'
+  #task :html => :spec
 end
+
+=begin
+RSpec::Core::RakeTask.new(:spec) do |t|
+  t.rspec_opts = '--format html --out reports/rspec_results.html'
+end
+
+namespace :rspec_report do
+  desc 'Run all specs and generate RSpec report in HTML'
+  task :html => :spec
+
+  desc 'Run all specs, generate RSpec report and open it in the browser'
+  task :browser do
+    Rake::Task[:spec].invoke
+    `open reports/rspec_results.html` # This only works if running OS X.
+  end
+end
+=end
